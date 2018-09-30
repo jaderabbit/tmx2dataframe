d9c75035b22201fa421ce80538c31159149496e6
Jade Abbott <jabbott@retrorabbit.co.za>
2018-09-30 15:24:35 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(name='tmx2dataframe',
-      version='0.1',
-      description='Converting tmx translation files to pandas dataframe',
-      url='http://github.com/jaderabbit/tmx2dataframe',
-      author='Jade Abbott',
-      author_email='jade.zoe.abbott@gmail.com',
-      license='MIT',
-      packages=['tmx2dataframe'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='tmx2dataframe',
+    version='0.1',
+    description='Converting tmx translation files to pandas dataframe',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/jaderabbit/tmx2dataframe',
+    author='Jade Abbott',
+    author_email='jade.zoe.abbott@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ])
---
+++ tmx2dataframe/__init__.py
@@ -0,0 +1,1 @@
+name = "tmx2dataframe"
